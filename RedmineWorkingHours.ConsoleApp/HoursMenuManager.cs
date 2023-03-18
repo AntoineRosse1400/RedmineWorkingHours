6cb8d5f5ff5488034aa7c9384c6e96b4fd7315f5
Antoine Rossé <antoine.rosse@adonite.com>
2023-03-18 20:44:57 +0100

    Implement menu

--- RedmineWorkingHours.ConsoleApp/HoursMenuManager.cs
+++ RedmineWorkingHours.ConsoleApp/HoursMenuManager.cs
@@ -26,6 +26,70 @@ internal class HoursMenuManager
 
     public void Run()
     {
+        PrintTitle();
+        bool mustExit;
+        do
+        {
+            mustExit = ExecuteOneMenuAction();
+        } while (!mustExit);
+    }
+
+    private void PrintTitle()
+    {
+        Console.WriteLine("==================== WORKING HOURS BALANCE ====================");
+    }
+
+    private bool ExecuteOneMenuAction()
+    {
+        PrintMenu();
+        return UserEntry();
+    }
+
+    private void PrintMenu()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Select the action to execute:");
+        Console.WriteLine("1. Hours balance until last month");
+        Console.WriteLine("2. Vacation days balance until now");
+        Console.WriteLine("3. Vacation days balance until end of year");
+        Console.WriteLine("0. Exit");
+    }
+
+    private bool UserEntry()
+    {
+        string? userInput = Console.ReadLine();
+        if (userInput == null)
+            return false;
+
+        if (!int.TryParse(userInput, out int actionIndex))
+            return false;
+
+        return ExecuteMenuAction(actionIndex);
+    }
+
+    private bool ExecuteMenuAction(int actionIndex)
+    {
+        switch (actionIndex)
+        {
+            case 0:
+                return true;
+            case 1:
+                this.PrintHoursBalance();
+                break;
+            case 2:
+                this.PrintVacationDaysBalanceUntilNow();
+                break;
+            case 3:
+                this.PrintVacationDaysBalanceUntilEndOfYear();
+                break;
+            default:
+                return false;
+        }
+        return false;
+    }
+
+    private void PrintHoursBalance()
+    {
         var appConfiguration = _serviceProvider.GetService<AppConfiguration>();
         if (appConfiguration == null)
             throw new NullReferenceException($"{nameof(AppConfiguration)} service not found.");
@@ -40,6 +104,16 @@ internal class HoursMenuManager
 
         double hoursBalance = hoursCalculator.GetHoursBalance(begin, end);
         Console.WriteLine($"Hours balance: {hoursBalance}");
+    }
+
+    private void PrintVacationDaysBalanceUntilNow()
+    {
+        Console.WriteLine("Working on it...");
+    }
+
+    private void PrintVacationDaysBalanceUntilEndOfYear()
+    {
+        Console.WriteLine("Working on it...");
     }
 
     #endregion
