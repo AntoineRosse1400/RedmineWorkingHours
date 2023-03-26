c3d27d26a5b93f25c7643c02791c97b1ccb90c13
Antoine Rossé <antoine.rosse@adonite.com>
2023-03-26 11:05:33 +0200

    Fix week days calculation

--- RedmineWorkingHours.ConsoleApp/Utils/DateTimeUtils.cs
+++ RedmineWorkingHours.ConsoleApp/Utils/DateTimeUtils.cs
@@ -4,8 +4,26 @@ internal static class DateTimeUtils
 {
     public static int GetWeekDays(DateTime begin, DateTime end)
     {
-        return (int)(1 + ((end - begin).TotalDays * 5 -
-                          (begin.DayOfWeek - end.DayOfWeek) * 2) / 7);
+        int days = (int)(end - begin).TotalDays + 1;
+        int fullWeeks = days / 7;
+        int weekdaysInFullWeeks = fullWeeks * 5;
+
+        int extraDays = days % 7;
+        int weekdaysInExtraDays = 0;
+
+        if (extraDays <= 0)
+            return weekdaysInFullWeeks;
+
+        DateTime currentDate = begin.AddDays(fullWeeks * 7);
+        while (extraDays > 0)
+        {
+            if (currentDate.DayOfWeek != DayOfWeek.Saturday && currentDate.DayOfWeek != DayOfWeek.Sunday)
+                weekdaysInExtraDays++;
+            currentDate = currentDate.AddDays(1);
+            extraDays--;
+        }
+
+        return weekdaysInFullWeeks + weekdaysInExtraDays;
     }
 
     public static DateTime GetFirstDateOfMonth(int year, int month)
